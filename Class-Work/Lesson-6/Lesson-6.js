b060bdffb1dcd30b0523acf879742c20e50b47f4
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-09 21:27:23 +0300

    JS. Приведення конвертація типів

---
+++ Class-Work/Lesson-6/Lesson-6.js
@@ -0,0 +1,24 @@
+// console.log(typeof (100500+''));
+// console.log(100+200+'');
+// console.log('' + 100+200);
+
+// console.log( + '100500asd');
+// console.log(parseInt('100500.123'));
+// console.log(parseFloat('100500.123'));
+
+
+// console.log(!!100);
+// console.log(!!0);
+//
+// console.log(!!'asd');
+// console.log(!!'');
+//
+// console.log(!!'0');
+
+
+console.log(true + '');
+console.log(+true);
+console.log(+false);
+console.log(100+true);
+
+
