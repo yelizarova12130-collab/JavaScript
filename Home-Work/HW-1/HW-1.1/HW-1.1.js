e3dfb8d2ad1adc6172a4b74b8bbc479f9b5de8fe
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-09 22:17:34 +0300

    HW-1.1

---
+++ Home-Work/HW-1/HW-1.1/HW-1.1.js
@@ -0,0 +1,37 @@
+// Створити змінні. Присвоїти кожному з них значення: ‘hello’,’owu’,’com’, ‘ua’, 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//
+// Вивести кожну змінну за допомогою: console.log
+
+let string1 =`hello`;
+let string2 =`owu`;
+let string3 =`com`;
+let string4 =`ua`;
+
+console.log(string1);
+console.log(string2);
+console.log(string3);
+console.log(string4);
+
+let number1 = 1;
+let number2 = 10;
+let number3 = -999;
+let number4 = 123;
+let number5 = 16;
+
+console.log(number1);
+console.log(number2);
+console.log(number3);
+console.log(number4);
+console.log(number5);
+
+let PI = 3.14;
+let float = 2.7;
+
+console.log(PI);
+console.log(float);
+
+let boolean1 = true;
+let boolean2 = false;
+
+console.log(boolean1);
+console.log(boolean2);
