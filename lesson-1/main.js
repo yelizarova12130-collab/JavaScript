74e235b257770021d769787724dabe63c501816e
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-09 19:58:54 +0300

    Змінні

---
+++ lesson-1/main.js
@@ -0,0 +1,14 @@
+console.log('Hello World!');
+
+// Має декілька базових типів
+// number
+//string
+//boolean
+
+const PI=3.14;
+console.log(PI);
+
+let a = 'sdfghjk';
+console.log(a);
+a='okten';
+console.log(a);
