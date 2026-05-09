b6218651af1085347b2be069d368aa0d75f5db15
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-09 22:34:17 +0300

    HW-1.3

---
+++ Home-Work/HW-1/HW-1.3/HW-1.3.js
@@ -0,0 +1,9 @@
+//– За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a,a);
+console.log(typeof b,b);
+console.log(typeof c,c);
