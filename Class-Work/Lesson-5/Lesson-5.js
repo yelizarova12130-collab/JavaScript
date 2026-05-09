b6183a8e3452311487cd1578a73d70e99ba11915
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-09 20:58:01 +0300

    JS. Булевий тип та операції з ним

---
+++ Class-Work/Lesson-5/Lesson-5.js
@@ -0,0 +1,12 @@
+let a = true;
+// console.log(typeof a,a);
+// > < >= <= == === != !==
+// console.log(10 > 5);
+// console.log(10 > 20);
+// console.log(10 === 10);
+// console.log(10 !== 10);
+
+console.log(5 === '5');
+console.log('asd' === 'asd');
+console.log('asd' > 'asdq');
+console.log(5 !== '6');
