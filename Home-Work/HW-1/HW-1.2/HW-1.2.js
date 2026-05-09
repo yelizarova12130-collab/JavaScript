f54499eae57bf45a5fe1df6dd87fad5dd9c034fb
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-09 22:26:54 +0300

    HW-1.2

---
+++ Home-Work/HW-1/HW-1.2/HW-1.2.js
@@ -0,0 +1,10 @@
+ // Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З’єднати їх в одну змінну person
+ // (Не об’єкт, просто за допомогою конкатенації)
+
+ let firstName = 'Валерія';
+ let middleName = 'Юріївна';
+ let lastName = 'Єлізарова';
+
+let person =`${firstName} ${middleName} ${lastName}` ;
+console.log(person);
+
