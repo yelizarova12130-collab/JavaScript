536cf898eb4cdc35d947721a137d84ce54367c6c
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-09 22:42:22 +0300

    HW-1.4

---
+++ Home-Work/HW-1/HW-1.4/HW-1.4.js
@@ -0,0 +1,11 @@
+// За допомогою 3-х різних prompt() отримати 3 слова які являються вашими Імʼям, По-батькові та роками. Та вивести в консоль
+
+let firstName = prompt("Введіть ваше ім'я");
+let middleName = prompt("Введіть ваше по-батькові");
+let age = prompt("Введіть ваш вік");
+
+console.log(firstName);
+console.log(middleName);
+console.log(age);
+
+
