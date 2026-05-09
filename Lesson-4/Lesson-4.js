b9ac4cd078d16a3d95ea0668653599aef21dcd36
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-09 20:37:58 +0300

    JS. Числа та операції з ними

---
+++ Lesson-4/Lesson-4.js
@@ -0,0 +1,22 @@
+// Integer - цілі числа
+let a = 100500;
+console.log(a);
+// floot double - числа з плаваючею крапкою
+let b = 3.123456789;
+console.log(b);
+
+console.log(20+10);
+console.log(20-10);
+console.log(20/10);
+console.log(20*10);
+// Ділення по модулю
+console.log(11%3);
+
+console.log(0.1+0.2);
+
+let big = BigInt(1234567890123456789012345678901234567890123456789);
+console.log(big);
+
+let n1 = 100;
+let n2 = 200;
+console.log(n1+n2);
