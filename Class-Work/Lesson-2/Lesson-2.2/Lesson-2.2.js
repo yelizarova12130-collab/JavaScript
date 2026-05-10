531e5088c3acecc3f7223408a629dd93884c8ed6
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-10 12:53:12 +0300

    JS. Багатовимірні масиви

---
+++ Class-Work/Lesson-2/Lesson-2.2/Lesson-2.2.js
@@ -0,0 +1,12 @@
+let arr = [
+    [11,22,33], //0
+    [], //1
+    [] //2
+];
+console.log(arr);
+//                                 0  1  2
+let innerArray = arr[0]; //11 22 33
+console.log(innerArray[1]);
+
+console.log(arr[0] [1]);
+
