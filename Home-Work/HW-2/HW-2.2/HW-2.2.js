5bc3c63bbb8d5324f1948cc558ffd0c86d5ad06f
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-10 23:32:43 +0300

    HW-2.2

---
+++ Home-Work/HW-2/HW-2.2/HW-2.2.js
@@ -0,0 +1,10 @@
+//– Створити 3 об’єкти які описують книги. Поля об’єкту : title ,pageCount, genre.
+
+let book1 = {title: 'puzzle', pageCount: 456, genre: 'crime story'};
+let book2 = {title: 'maze', pageCount: 678, genre: 'detective'};
+let book3 = {title: 'unfinished manuscript', pageCount: 234, genre: 'detective'};
+
+console.log(book1);
+console.log(book2);
+console.log(book3);
+
