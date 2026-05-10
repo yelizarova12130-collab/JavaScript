46b5626a5bd0fa75cac79d5a41c72e17c1df4026
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-10 13:17:26 +0300

    JS. Об’єкти,JS. Внутрішні об’єкти

--- Class-Work/Lesson-2/Lesson-2.3/Lesson-2.3.js
+++ Class-Work/Lesson-2/Lesson-2.3/Lesson-2.3.js
@@ -7,6 +7,20 @@
 let user1 = {
     id : 1,
     name : "Vasya",
-    age : 31
+    age : 31,
+    skills: ['java', 'js', 'html'],
+    wife: {
+        name : "Olya",
+        age : 28
+    }
 }
-console.log(user1);
+// console.log(user1);
+//
+// //Витягнути будь яку з характеристик (перший спосіб)
+// console.log(user1.id);
+// //(Другий спосіб) У випадку якщо ми хочемо звернутись через масиви
+// console.log(user1['age']);
+//
+// console.log(user1.skills[0]);
+
+console.log(user1.wife.name);
