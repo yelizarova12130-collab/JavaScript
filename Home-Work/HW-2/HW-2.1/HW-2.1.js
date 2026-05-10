9acd5f225e40e95af97ff7cd77dd3ad64cf48bef
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-10 23:08:41 +0300

    HW-2.1

---
+++ Home-Work/HW-2/HW-2.1/HW-2.1.js
@@ -0,0 +1,25 @@
+//Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+let arr = [
+    33,
+    'name',
+    3.14,
+    true,
+    {value:1},
+    [],
+    null,
+    NaN,
+    0,
+    false
+]
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
