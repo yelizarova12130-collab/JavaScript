9270b28db1bfd01a87781018ec86bf58db941260
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-10 16:47:05 +0300

    JS. Тернарний оператор

---
+++ Class-Work/Lesson-2/Lesson-2.9/Lesson-2.9.js
@@ -0,0 +1,20 @@
+let obj = {
+    id: 1,
+    name: 'Vasya',
+    age: 16
+}
+
+// let access;
+//
+// if(obj.age > 18) {
+//     access=true;
+// }else{
+//     access=false;
+// }
+//
+// console.log(access);
+
+let access=obj.age>18?true:false;
+
+
+console.log(access);
