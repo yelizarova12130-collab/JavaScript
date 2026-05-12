cd5f722a7b906989f46df6bc9860ddd0f1881f38
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-12 15:40:37 +0300

    HW-2.7

--- Home-Work/HW-2/HW-2.7/HW-2.7.js
+++ Home-Work/HW-2/HW-2.7/HW-2.7.js
@@ -1,14 +1,14 @@
 // – Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
 // (в першу, другу, третю або четверту частину години).
 
-let time = 59;
+let time = 0;
 
-if(time <=15){
+if( time >0 && time <=15){
     console.log('Перша Чверть');
-}else if(time <= 30){
+}else if(time >15 && time <= 30){
     console.log('Друга Чверть');
-}else if(time <= 45){
+}else if(time >30 && time <= 45){
     console.log('Третя Чверть');
-}else if(time <= 60){
+}else if(time >45 && time <= 60){
     console.log('Четверта Чверть');
 }
