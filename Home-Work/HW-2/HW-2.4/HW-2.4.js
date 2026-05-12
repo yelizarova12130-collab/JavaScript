da6870cb757a9c8caba0236876ccb55d005278d2
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-12 13:08:54 +0300

    HW-2.4

---
+++ Home-Work/HW-2/HW-2.4/HW-2.4.js
@@ -0,0 +1,28 @@
+//– Створити масив з 10 об’єктами, які описують сутніть “користувач”.
+// Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+let users = [
+     {name: 'Alexander', username: 'oleksandr_koval', password: 'G7!kLp#9qR'},
+     {name: 'Irene', username: 'iryna_bond', password: 'vF5&mNx2$z'},
+     {name: 'Michael', username: 'mykhailo_shev', password: 'Xc9#wRt!3Y'},
+     {name: 'Natalie', username: 'natalia_lysenko', password: 'pL4$zKv#7M'},
+     {name: 'Dmitro', username: 'dmytro_gavr', password: 'Hs6@qWp!1F'},
+     {name: 'Helen', username: 'olena_tkachenko', password: 'eR3&yUm%9Z'},
+     {name: 'Andrew', username: 'andriy_melnyk', password: 'Wv8#bNx!4Q'},
+     {name: 'Victoria', username: 'viktoriya_polis', password: 'M2$jFk@7L'},
+     {name: 'Sergey', username: 'serhiy_roman', password: 'zC5&wPq#6V'},
+     {name: 'Tatiana', username: 'tetiana_savchuk', password: 'aB9@rXs%1K'},
+];
+
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
+
+
