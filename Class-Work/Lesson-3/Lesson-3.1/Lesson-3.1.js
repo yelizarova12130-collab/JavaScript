b11cdc062e03978c916ac1a6de840a210ebe065d
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-13 12:04:12 +0300

    JS. Інкремент / декременти

---
+++ Class-Work/Lesson-3/Lesson-3.1/Lesson-3.1.js
@@ -0,0 +1,37 @@
+// Інкременти (збільшення) але тільки на одиницю
+
+// let x = 0;
+// x = x + 1; //x=1
+// console.log(x);
+//
+// x ++;
+// console.log(x);
+//
+// x += 1;
+// console.log(x);
+//
+// // Декрименти (зменшення) але тільки на одиницю
+//
+// x = x - 1; //2
+// console.log(x);
+// x -= 1; //1
+// console.log(x);
+// x--;
+// console.log(x);
+
+// Різновиди декрементів
+
+// let x = 0;
+// x++; //postdecrement
+// ++x; //префіксний
+// console.log(x);
+
+//
+// let a = 0;
+// let b = a++;
+// console.log(a, b); // a=1 b=0
+
+
+let a = 0;
+let b = ++a;
+console.log(a, b); //a=1 b=1
