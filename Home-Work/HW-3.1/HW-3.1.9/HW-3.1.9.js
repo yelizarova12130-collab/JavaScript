8cc655eea63bc64913c18e1788a7f0d2c2039ba6
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-18 14:05:36 +0300

    HW-3.1.9

---
+++ Home-Work/HW-3.1/HW-3.1.9/HW-3.1.9.js
@@ -0,0 +1,39 @@
+// – Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+
+// for (let i = 0; i<10; i++){
+//     console.log(i);
+//     document.write(i+' ');
+// }
+
+//– Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+
+// for (let i = 0; i<100; i++){
+//     console.log(i);
+//     document.write(i+' ');
+// }
+
+//– Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
+
+// for (let i = 0; i<100; i++){
+//     console.log(i);
+//     document.write(i+ ' ');
+//     i++;
+// }
+
+
+//– Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
+
+// for (let i = 0; i<100; i++){
+//     if (i % 2 === 0){
+//         console.log(i);
+//         document.write(i+ ' ');
+//     }
+// }
+
+//– Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
+// for (let i = 1; i<100; i++){
+//         console.log(i);
+//         document.write(i+ ' ');
+//         i++;
+// }
+
