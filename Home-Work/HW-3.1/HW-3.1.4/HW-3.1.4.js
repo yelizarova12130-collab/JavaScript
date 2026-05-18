41e192614f8b00e0b08cf5f169cf6db99e089e4b
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-18 13:05:48 +0300

    HW-3.1.4

---
+++ Home-Work/HW-3.1/HW-3.1.4/HW-3.1.4.js
@@ -0,0 +1,13 @@
+//– Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
+
+let numbers = [12, -3, 0, 3.14, 5, 5.15, 10, 10000, 1234567890, 9];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+
+
+for (number of numbers) {
+    console.log(number);
+}
