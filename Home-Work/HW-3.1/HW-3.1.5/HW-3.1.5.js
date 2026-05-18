58d04096757c8fd5cc96835680566401bb57885d
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-18 13:12:27 +0300

    HW-3.1.5

---
+++ Home-Work/HW-3.1/HW-3.1.5/HW-3.1.5.js
@@ -0,0 +1,11 @@
+// – Створити масив з 10 строкових елементів. Вивести в консоль всі його елементи в циклі.
+
+let strings = ['Anna', 'Oleg', 'Maria', 'Ivan', 'Sofia', 'Petya', 'Oksana', 'Dmitry', 'Julia', 'Andrey'];
+
+// for (string of strings) {
+//     console.log(string);
+// }
+
+for (let i = 0; i < 10; i++) {
+    console.log(strings[i]);
+}
