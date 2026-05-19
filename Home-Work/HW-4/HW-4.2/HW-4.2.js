394dc63d819f8b3e100b169f7d2d9400ca296ae8
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-19 12:56:23 +0300

    HW-4.2

---
+++ Home-Work/HW-4/HW-4.2/HW-4.2.js
@@ -0,0 +1,7 @@
+// створити функцію, яка обчислює та повертає площу кола з радіусом r
+
+function square (radius) {
+    return result = Math.PI * ( radius * radius );
+}
+let number = square(4);
+console.log(number);
