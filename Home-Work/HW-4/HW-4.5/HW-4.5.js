2e958870aa2d23b57e11e2ca8748a53ef92f03df
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-19 13:35:31 +0300

    HW-4.5

---
+++ Home-Work/HW-4/HW-4.5/HW-4.5.js
@@ -0,0 +1,14 @@
+// створити функцію, яка створює параграф з текстом та виводить його через document.write.
+// Текст задати через аргумент
+
+function paragraph (text) {
+    document.write(`<p> ${text} </p>`);
+}
+
+paragraph('I wish you a wonderful day,');
+paragraph(' May the sun shine bright and warm your way.');
+paragraph('May luck stay with you all day through,');
+paragraph( 'Without sadness, stress, or feeling blue.');
+paragraph( 'And may your heart feel light and bright 🌷');
+
+
