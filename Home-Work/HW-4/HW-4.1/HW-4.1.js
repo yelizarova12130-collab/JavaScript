3d1a4b0827004eaa47627ab9ed34144552da3de7
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-19 12:45:56 +0300

    HW-4.1

---
+++ Home-Work/HW-4/HW-4.1/HW-4.1.js
@@ -0,0 +1,8 @@
+//створити функцію, яка обчислює та повертає площу прямокутника зі сторонами  а і б
+
+function square (a,b) {
+    return result = a*b;
+}
+
+let number = square(2,4);
+console.log(number);
