d3fb2c8880fc59f4839cb76f399c15dc7b21351f
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-20 21:09:09 +0300

    HW-6.3

---
+++ Home-Work/HW-6/HW-6.3/HW-6.3.js
@@ -0,0 +1,8 @@
+// Перевести до нижнього регістру наступні стрінгові значення
+//
+//       ‘HELLO WORLD’, ‘LOREM IPSUM’, ‘JAVASCRIPT IS COOL’
+
+let str = ['HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'];
+console.log(str[0].toLowerCase());
+console.log(str[1].toLowerCase());
+console.log(str[2].toLowerCase());
