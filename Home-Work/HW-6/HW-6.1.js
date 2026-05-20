9ae924f92ae829dbe9d695b758c53de048e1a4c2
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-20 21:00:14 +0300

    HW-6.1

---
+++ Home-Work/HW-6/HW-6.1.js
@@ -0,0 +1,7 @@
+// – Знайти та вивести довжину наступних стрінгових значень
+
+let arr = ['hello world', 'lorem ipsum', 'javascript is cool'];
+
+console.log(arr[0].length);
+console.log(arr[1].length);
+console.log(arr[2].length);
