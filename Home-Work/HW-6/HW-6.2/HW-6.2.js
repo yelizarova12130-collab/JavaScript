94025f69630e1f8b4df9dbcb89495fed6e09e77a
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-20 21:06:13 +0300

    HW-6.2

---
+++ Home-Work/HW-6/HW-6.2/HW-6.2.js
@@ -0,0 +1,7 @@
+// Перевести до великого регістру наступні стрінгові значення
+//       ‘hello world’, ‘lorem ipsum’, ‘javascript is cool’
+
+let str = ['hello world', 'lorem ipsum', 'javascript is cool'];
+console.log(str[0].toUpperCase());
+console.log(str[1].toUpperCase());
+console.log(str[2].toUpperCase());
