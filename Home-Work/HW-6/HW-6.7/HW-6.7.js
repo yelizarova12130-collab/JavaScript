b5dbbd411ec227eb96035f7033c8b744af365cc1
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-21 16:20:11 +0300

    HW-6.7

---
+++ Home-Work/HW-6/HW-6.7/HW-6.7.js
@@ -0,0 +1,20 @@
+// створити функцію sortNums(array,direction), яка приймає масив чисел, та сортує його від більшого до меншого,
+// або навпаки – залежно від значення аргументу direction.
+
+// let nums = [11,21,3];
+//
+// sortNums(nums,’ascending’) // [3,11,21]
+//
+// sortNums(nums,’descending’) // [21,11,3]
+
+
+let nums = [11,21,3];
+
+function sortNums(array,direction) {
+    if (direction === 'ascending') return array.sort((a,b)=> a-b);
+   if (direction === 'descending') return array.sort((a,b)=> b-a);
+}
+
+console.log(sortNums(nums,'ascending'));
+console.log(sortNums(nums,'descending'));
+
