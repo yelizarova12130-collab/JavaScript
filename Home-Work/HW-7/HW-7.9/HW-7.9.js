2661a0e661a1c573a1e4a3f82d8ff2d409cdd234
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-05-27 10:55:27 +0300

    HW-7.9

---
+++ Home-Work/HW-7/HW-7.9/HW-7.9.js
@@ -0,0 +1,42 @@
+// *Через Array.prototype. створити власний foreach, filter
+
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+
+Array.prototype.myForEach = function (callback) {
+
+    for (const item of this) {
+        callback(item);
+    }
+
+};
+
+users.myForEach (function (item){
+    console.log(item);
+});
+
+Array.prototype.myFilter = function (callback) {
+    const result = [];
+    for (const item of this) {
+
+        if (callback(item)) {
+            result.push(item);
+        }
+
+    }
+    return result;
+};
+
+const result = users.myFilter((user) => user.age < 30);
+console.log(result);
