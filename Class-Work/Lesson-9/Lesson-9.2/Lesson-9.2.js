290af33f1aec13c5bdfa3a0aae6ddab0573610c1
yelizarova12130-collab <yelizarova12130@gmail.com>
2026-06-16 14:44:11 +0300

    JS. Forms API

---
+++ Class-Work/Lesson-9/Lesson-9.2/Lesson-9.2.js
@@ -0,0 +1,7 @@
+console.log(document.forms);
+// Для того щоб взяти ту чи іншу форму
+
+console.log(document.forms['f1']);
+console.log(document.forms['f1'].userName);
+
+console.log(document.forms.f2.userEmail);
